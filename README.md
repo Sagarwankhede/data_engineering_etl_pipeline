a27d1daa5ae252b5cbff2370c06374301633183f
Sagarwankhede <sagarwankhede58@gmail.com>
2024-12-29 01:14:28 +0530

    Added test cases

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Trying out ETL pipeline on UFO Dataset because why not?
 
-![UFO](doc/ufo.jpeg)
+![UFO](docs/ufo.jpeg)
---
+++ tests/unit/test_extract.py
@@ -0,0 +1,8 @@
+from unittest.mock import patch
+import pytest
+# from ufo_data_etl.etl_pipeline.extract import download_dataset
+
+# @patch("ufo_data_etl.etl_pipeline.extract.kaggle_api.dataset_download_files")
+def test_download_dataset():
+
+    assert 1 == 1
